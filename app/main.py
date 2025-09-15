c7b7a9c6eccd926e52eb41d5e2b13d1aa709a4c9
HassanSidd0946 <hassansiddiqui0946@gmail.com>
2025-09-15 18:41:28 +0500

    UPDATED

--- app/main.py
+++ app/main.py
@@ -6,3 +6,16 @@ from app.api import routes_auth, routes_predict
 from app.middleware.logging_middleware import LoggingMiddleware
 from app.core.exceptions import register_exception_handlers
 
+app = FastAPI(title = "Car Price Prediction")
+
+# link middleware
+app.add_middleware(LoggingMiddleware)
+
+# link endpoints
+app.include_router(routes_auth.router, tags=['Auth'])
+app.include_router(routes_predict.router, tags=['Prediction'])
+
+# monitoring using Prometheus
+Instrumentator().instrument(app).expose(app)
+
+# add exception handler
